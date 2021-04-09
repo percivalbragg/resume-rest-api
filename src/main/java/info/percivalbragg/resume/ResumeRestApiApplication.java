d5da38bd2eb794683a3e3f1ffa77a273aa7ede90
percivalbragg <percivalbragg@yahoo.com>
2021-04-09 15:03:28 -0500

    Initial commit

---
+++ src/main/java/info/percivalbragg/resume/ResumeRestApiApplication.java
@@ -0,0 +1,13 @@
+package info.percivalbragg.resume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/percivalbragg/resume/ResumeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package info.percivalbragg.resume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
